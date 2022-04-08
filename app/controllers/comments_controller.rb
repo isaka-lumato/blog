c98c2d60a24e9c107f21f0c9fb58717b967191cb
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2022-04-08 19:20:56 +0300

    Fix add comment form and Edit readMe

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,4 @@
 class CommentsController < ApplicationController
-  # def create
-  #   @comment = current_user.comments.new(comment_params)
-
-  #   respond_to do |format|
-  #     if @comment.save
-  #       format.html { redirect_to user_posts_path(current_user) }
-  #     else
-  #       format.html { render :show }
-  #     end
-  #   end
-  # end
-
-  # private
-
-  # def comment_params
-  #   params.require(:comment).permit(:text)
-  # end
-
   def new
     @comment = Comment.new
   end
